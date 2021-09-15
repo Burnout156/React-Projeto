ab7dfba34d8eda188744adcc8388984913b3efe7
burnout156 <brunoferreira378@gmail.com>
2021-09-15 15:52:36 -0300

    refeito tudo

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
 import logo from './logo.svg';
-import './css/App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Button from './components/Button'
+import './App.css';
 
-function App() 
-{
+function App() {
   return (
     <div className="App">
-      {/* <Greet name = "Bruno" heroName = "Fortão"> 
-        <Hello>
-        </Hello>
-      </Greet>
-      <Greet name = "Guacirema" heroName = "Gordinha">
-        <Hello>
-        </Hello>
-      </Greet>
-
-      <Button onClick = {() => Soma(10, 1)} name = "Some"/> {/*primeiro passo a função e depois o nome do botão*/}
-
-
-      {/* <MyComponent/>*/}
-      {/*<Welcome/>*/}
-      {/*<Hello/>*/}
-      <Welcome name = "Bruno" heroName = "Fortão"/> {/*exemplo de como passar um parâmetro direto sem div */}
-      <Welcome name = "Guacirema"/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  ); 
-}
-
-function Soma(a, b)
-{
-    alert(a + b);
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Operators from './components/Operators'
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render( <Operators/>,  document.getElementById('root'));
-/*aqui eu coloquei o operators afim de executar a página do operators, mas poderia deixar o App */
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
